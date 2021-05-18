cb2733b7750cc999fb85f4f980a962e1da07676e
erikasinio <erikaeliza.sinio@gmail.com>
2021-05-18 16:09:07 +0800

    Added delete feature and modified code

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   root 'pages#home'
-  resources :articles, only: [:show, :index, :new, :create, :edit, :update]
+  resources :articles
   # get '/articles' => 'articles#index'
   # get '/articles/new' => 'articles#new', as: 'new_article'
   # post '/articles' => 'articles#create', as: 'create_article'
