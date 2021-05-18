3ca175ed72a915b6839ebc139229d96faab7f428
erikasinio <erikaeliza.sinio@gmail.com>
2021-05-18 14:54:17 +0800

    Added show functionality

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,7 +1,8 @@
 class Article < ApplicationRecord
 
     validates :title, presence: true,
+                      length: {minimum: 5, maximum: 100},  
                       uniqueness: true
     validates :body, presence: true,
-                      length: {minimum: 5}  
+                      length: {minimum: 10, maximum: 300}  
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   root 'pages#home'
+  resources :articles, only: [:show]
   get '/articles' => 'articles#index'
   get '/articles/new' => 'articles#new', as: 'new_article'
   post '/articles' => 'articles#create', as: 'create_article'
