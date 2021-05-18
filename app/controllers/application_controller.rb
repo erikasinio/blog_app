fefac9fcc1d8eff470539ca266e57f8d8a992909
erikasinio <erikaeliza.sinio@gmail.com>
2021-05-18 14:01:28 +0800

    Added articles and home page

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,33 @@
 class ApplicationController < ActionController::Base
+
+    def hello
+      render html:'Hello World!'
+    end
+
+    def index
+        @articles = Article.all
+      end
+    #   def show
+    #     @article = Article.find(params[:id])
+    #     @comments = @article.comments
+    #   end
+      def new
+        @article = Article.new
+        @article.title = params[:title]
+        @article.body = params[:body]
+      end
+
+      def create
+        @article = Article.new(article_params)
+        if @article.save
+          redirect_to articles_path
+        else
+          render :new
+        end
+      end
+      
+      private
+      def article_params
+        params.require(:article).permit(:title, :body)
+      end
 end
---
+++ app/models/article.rb
@@ -0,0 +1,7 @@
+class Article < ApplicationRecord
+
+    validates :title, presence: true,
+                      uniqueness: true
+    validates :body, presence: true,
+                      length: {minimum: 5}  
+end
